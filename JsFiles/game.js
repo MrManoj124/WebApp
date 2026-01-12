73ab1991a06b46de6154184a4b39d3b30b28d563
MrManoj124 <manojmanorooban@gmail.com>
2026-01-12 13:01:30 +0530

    Firstly get all the ids

---
+++ JsFiles/game.js
@@ -0,0 +1,6 @@
+const question = document.getElementById('question');
+const choices = document.getElementsByClassName('choice-text');
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBaarFull');
+
