1a934bb3ca7a65e41ea09e53a70dbb81b0b20b9f
MrManoj124 <manojmanorooban@gmail.com>
2026-01-12 13:03:39 +0530

    initialise the variables

--- JsFiles/game.js
+++ JsFiles/game.js
@@ -4,3 +4,12 @@ const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBaarFull');
 
+let curretQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+// let questions = [{
+//     question : "What"
+// }]
