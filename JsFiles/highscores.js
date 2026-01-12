01670d7183f983ea0f24314a06d1638d33677e95
MrManoj124 <manojmanorooban@gmail.com>
2026-01-12 14:06:06 +0530

    modify the game.js file

--- JsFiles/highscores.js
+++ JsFiles/highscores.js
@@ -1,13 +1,13 @@
-const highScoresList = document.getElementById('highScores');
+const highScoresList = document.getElementById('highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 highScoresList.innerHTML = highScores
-.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-})
-.join('');
+    .map(score => {
+        return `<li class="high-score">${score.name} - ${score.score}</li>`;
+    })
+    .join('');
 
-//If no scores yet, display a message
-if(highScores.length === 0){
-    highScoresList.innerHTML = '<li class="high-score"> No Scores yet.Play the quiz!</li>';
+// If no scores yet, display a message
+if (highScores.length === 0) {
+    highScoresList.innerHTML = '<li class="high-score">No scores yet. Play the quiz!</li>';
 }
