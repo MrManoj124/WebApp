9f39381d45333d3f576f123080265c6a24e6ceec
MrManoj124 <manojmanorooban@gmail.com>
2026-01-13 23:42:19 +0530

    modify the game.js file

--- JsFiles/game.js
+++ JsFiles/game.js
@@ -114,29 +114,30 @@ getNewQuestion = () =>{
         choice.innerText = currentQuestion['choice' + (index + 1)];
     });
 
-    choices.forEach(choice => {
-        choice.addEventListener('click', e =>{
-            if(!acceptingAnswers) return;
-
-            acceptingAnswers = false;
-            const selectedChoice = e.target;
-            const selectedAnswer = selectedChoice.dataset['number'];
-
-            const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-
-            if(classToApply === 'correct'){
-                incrementScore(CORRECT_BONUS);
-            }
-            selectedChoice.parentElement.classList.add(classToApply);
-
-            setTimeout(() => {
-                selectedChoice.parentElement.classList.remove(classToApply);
-                getNewQuestion();
-            }, 1000);
-        });
-    });
-
     acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e =>{
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if(classToApply === 'correct'){
+            incrementScore(CORRECT_BONUS);
+        }
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
 };
 
 incrementScore = num => {
